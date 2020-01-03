e1aad1510dcf3b69631115fa04aef3a575b4ba07
Jasmine Terry <jasmine.terry91@gmail.com>
2020-01-03 11:00:33 -0500

    cell styling for color/shadow

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -1,13 +1,13 @@
-const TETROMINOS = {
+export const TETROMINOS = {
     0:{ shape: [[0]], color:'0, 0, 0'},
 
     I: {
          shape: [
     
-            [0, 'I', 0,0],
-            [0, 'I',0, 0],
             [0, 'I', 0, 0],
-            [0, 'I',0, 0]
+            [0, 'I', 0, 0],
+            [0, 'I', 0, 0],
+            [0, 'I', 0, 0]
          ], 
          color: '80, 227, 230',
 
@@ -17,8 +17,8 @@ const TETROMINOS = {
         shape: [
     
             [0, 'J', 0],
-            [0, 'J',0],
-            ['J', 'J', 0]
+            [0, 'J', 0],
+            ['J','J', 0]
     
          ], 
          color: '36, 95, 223',
@@ -35,7 +35,7 @@ const TETROMINOS = {
          color: '223, 173, 36',
 },
 
-O:{
+O: {
     shape: [
 
         ['O', 'O'],
@@ -85,7 +85,8 @@ Z:{
 export const randomTetromino = () =>{
     const tetrominos = 'IJLOSTZ';
     const randTetromino = 
-    tetrominos[Math.floor(Math.random() * tetrominals.length)];
+    tetrominos[Math.floor(Math.random() * tetrominos.length)];
 
     return TETROMINOS[randTetromino];
 }
+
